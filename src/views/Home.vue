9780f4cde3c0301310db0095deaa898b33d7e713
stripedbug <49484866+stripedbug@users.noreply.github.com>
2023-05-08 01:51:46 +0300

    n

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,18 +7,18 @@
 </div></div>
 
 <div class="col-12 mt-3">
-<h2>Very easy to use!</h2>
+<h2 class="font-normal">Very easy to use!</h2>
 <br/>
 
-<h2>1. First create collections like "Office" or "HR" on expense collections "<i class="pi pi-fw pi-sitemap text-xl"></i>" tab.</h2>
+<h2 class="font-normal">1. First create collections like "Office" or "HR" on expense collections "<i class="pi pi-fw pi-sitemap text-xl"></i>" tab.</h2>
 
-<h2>2. Then add expenditure items to the collectionslike "Building Management" or "Electricity" on expenditure items "<i class="pi pi-fw pi-sort-alpha-down text-xl"></i>"  tab.</h2>
+<h2 class="font-normal">2. Then add expenditure items to the collectionslike "Building Management" or "Electricity" on expenditure items "<i class="pi pi-fw pi-sort-alpha-down text-xl"></i>"  tab.</h2>
 
-<h2>3. Then start adding expenses on expenses "<i class="pi pi-fw pi-money-bill text-xl"></i>"  tab. You can upload invoices too.</h2>
+<h2 class="font-normal">3. Then start adding expenses on expenses "<i class="pi pi-fw pi-money-bill text-xl"></i>"  tab. You can upload invoices too.</h2>
 
-<h2>- Optionally you can create custom tags for expenses on tags "<i class="pi pi-fw pi-tags text-xl"></i>"  tab to mark each expense.</h2>
+<h2 class="font-normal">- Optionally you can create custom tags for expenses on tags "<i class="pi pi-fw pi-tags text-xl"></i>"  tab to mark each expense.</h2>
 
-<h2>4. Then visit reports <i class="pi pi-fw pi-chart-bar"></i>  tab to view and download expense reports..</h2>
+<h2 class="font-normal">4. Then visit reports <i class="pi pi-fw pi-chart-bar"></i>  tab to view and download expense reports..</h2>
 
 
 </div>
