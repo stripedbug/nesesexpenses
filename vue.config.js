31bb2d64846c1f2469de7468b0d5f176da3c5245
stripedbug <49484866+stripedbug@users.noreply.github.com>
2023-05-14 08:16:17 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,17 +2,12 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-        name: "nesesxpenses",
+        name: "Neşe's Expenses",
         themeColor: '#ec4899',
         msTileColor: '#ec4899',
         appleMobileWebAppCapable: 'no',
         appleMobileWebAppStatusBarStyle: 'default',
-        manifestPath: 'manifest.json',
-        workboxPluginMode: 'InjectManifest',
         workboxOptions: {
-            // swSrc is required in InjectManifest mode.
-            swSrc: 'service-worker.js',
-            // ...other Workbox options...
             exclude: [/_redirects/],
         }
     },
