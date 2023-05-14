5b8c7e91d2fb98a76b9f1fe6721285741af40bda
stripedbug <49484866+stripedbug@users.noreply.github.com>
2023-05-14 08:14:43 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
-        name: "Neşe's Expenses",
+        name: "nesesxpenses",
         themeColor: '#ec4899',
         msTileColor: '#ec4899',
         appleMobileWebAppCapable: 'no',
